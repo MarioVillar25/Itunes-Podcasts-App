df41e6dacc203b385404148ef5692364523e9640
Mario Villar Lopez <mario.villar@nter.es>
2024-07-15 12:13:12 +0200

    Initial commit

---
+++ src/app/pages/episode-page/episode-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-episode-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './episode-page.component.html',
+  styleUrl: './episode-page.component.scss'
+})
+export class EpisodePageComponent {
+
+}
---
+++ src/app/pages/podcast-list/podcast-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-podcast-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './podcast-list.component.html',
+  styleUrl: './podcast-list.component.scss'
+})
+export class PodcastListComponent {
+
+}
