a99d3793e4fb38a3f63c73483a3a72c976d1c4df
Mario Villar Lopez <mario.villar@nter.es>
2024-07-15 13:53:42 +0200

    Settings and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path:'',
+        loadComponent: () =>
+          import('./pages/podcast-list/podcast-list.component').then(
+            (c) => c.PodcastListComponent
+          ),
+      },
+      {
+        path:'podcast/:podcastId',
+        loadComponent: () =>
+          import('./pages/episodes-list/episodes-list.component').then(
+            (c) => c.EpisodesListComponent
+          ),
+      },
+      {
+        path:'podcast/:podcastId/episode/:episodeId',
+        loadComponent: () =>
+          import('./pages/episode-page/episode-page.component').then(
+            (c) => c.EpisodePageComponent
+          ),
+      },
+      {
+        path: '**',
+        redirectTo: '',
+      }
+    ]
+  }
+];
