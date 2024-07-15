cc7209129dc47de1acba4ba9a20bc7fa0e279556
Mario Villar Lopez <mario.villar@nter.es>
2024-07-15 14:57:01 +0200

    structure landing-page & basic css layout

--- src/app/pages/podcast-list/podcast-list.component.ts
+++ src/app/pages/podcast-list/podcast-list.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { SearchBoxComponent } from "../../components/search-box/search-box.component";
 
 @Component({
   selector: 'app-podcast-list',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, SearchBoxComponent],
   templateUrl: './podcast-list.component.html',
   styleUrl: './podcast-list.component.scss'
 })
